b7960268dab0b91bbe8984d382af2ba4a1419f4c
AntoineRb <antoine.robert7700@gmail.com>
2022-03-15 12:59:58 +0100

    Change Relation In Database Models

--- prisma/migrations/20220315115710_init_database/migration.sql
+++ prisma/migrations/20220315115710_init_database/migration.sql
@@ -17,6 +17,7 @@ CREATE TABLE "Detail" (
     "operatingTime" TIMESTAMP(3) NOT NULL,
     "moduleState" BOOLEAN NOT NULL DEFAULT true,
     "dataCount" INTEGER NOT NULL DEFAULT 0,
+    "moduleId" INTEGER NOT NULL,
 
     CONSTRAINT "Detail_pkey" PRIMARY KEY ("id")
 );
@@ -24,6 +25,7 @@ CREATE TABLE "Detail" (
 -- CreateTable
 CREATE TABLE "Logs" (
     "id" SERIAL NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "value" DOUBLE PRECISION NOT NULL,
     "unit" VARCHAR(25) NOT NULL,
     "operatingTime" TIMESTAMP(3) NOT NULL,
@@ -37,10 +39,10 @@ CREATE TABLE "Logs" (
 CREATE UNIQUE INDEX "Module_name_key" ON "Module"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Module_infoId_key" ON "Module"("infoId");
+CREATE UNIQUE INDEX "Detail_moduleId_key" ON "Detail"("moduleId");
 
 -- AddForeignKey
-ALTER TABLE "Module" ADD CONSTRAINT "Module_infoId_fkey" FOREIGN KEY ("infoId") REFERENCES "Detail"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Detail" ADD CONSTRAINT "Detail_moduleId_fkey" FOREIGN KEY ("moduleId") REFERENCES "Module"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Logs" ADD CONSTRAINT "Logs_moduleId_fkey" FOREIGN KEY ("moduleId") REFERENCES "Module"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
