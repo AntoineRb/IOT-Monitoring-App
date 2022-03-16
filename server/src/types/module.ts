896037d30be98aa189cf9d568658914e5defcee1
AntoineRb <antoine.robert7700@gmail.com>
2022-03-16 10:52:24 +0100

    ADD Modules Types Enum

---
+++ server/src/types/module.ts
@@ -0,0 +1,7 @@
+export enum moduleTypes {
+    MOTOR = "motor",
+    ULTRASONIC_SENSOR = "ultrasonic_sensor",
+    BUETOOTH = "bluetooth",
+    CAMERA = "camera",
+    INFRARED_CAMERA = "infrared_camera"
+}
