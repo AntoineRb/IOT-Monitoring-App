07a576ae6fb99c5fbe596bd9e5aa359dd5a3b13a
AntoineRb <antoine.robert7700@gmail.com>
2022-03-18 13:51:21 +0100

    Replace getModulesList request to services directory

--- frontend/src/src/App.tsx
+++ frontend/src/src/App.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { IModule } from './Types/interface';
 import './App.scss';
 import Footer from './Components/Footer/Footer';
 import NavBar from './Components/NavBar/NavBar';
 import Router from './Router/Router';
 
 function App() {
+  const [ modulesList, setAllModules ]  = useState<IModule>();
+  const setModulesListState = ( newModulesList: IModule | undefined ) => {
+    if ( newModulesList !== undefined ) {
+      setAllModules( newModulesList );
+    } 
+    else {
+      const unexpectedType = typeof newModulesList;
+      console.error(`Modules.tsx: The value type expected is IModule but the actual type is ${unexpectedType}`);
+    }
+  }
+
+  useEffect(() => {
+    const getModulesList =  async() => {
+      await fetch('http://localhost:8080/module/all', { // Change Later For Base URL
+        method: "GET",
+        headers: {
+            "Content-Type":  "application/json; charset=UTF-8"
+        }
+      })
+      .then( response => response.json() )
+      .then(( response ) => {
+          setModulesListState( response );
+          console.log( response );
+      })
+      .catch(( err:Error ) => {
+          throw err;
+      });
+    }
+    window.addEventListener('load', async () => await getModulesList() );     
+    return () => window.removeEventListener('load', getModulesList );
+  }, []);
+
   return (
     <div className="App">
       <NavBar/>
---
+++ frontend/src/src/Containers/Modules/ModulesTable/ModulesTable.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface ITableProps {
+}
+
+const Table: React.FunctionComponent<ITableProps> = (props) => {
+    return (
+        <table>
+            
+        </table>
+    );
+};
+
+export default Table;
