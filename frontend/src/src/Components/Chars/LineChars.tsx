b10e8130515fbd88df721da0d29193a685f89411
AntoineRb <antoine.robert7700@gmail.com>
2022-03-20 21:03:55 +0100

    ADD React-Google-Charts

---
+++ frontend/src/src/Components/Chars/LineChars.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {Chart} from 'react-google-charts';
+
+interface IAppProps {
+}
+
+const App: React.FunctionComponent<IAppProps> = (props) => {
+  return <></>;
+};
+
+export default App;
